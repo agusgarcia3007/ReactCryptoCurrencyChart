777fbb69a2208a9fb0e65b45ed62b565420c3aa6
Agustin García dos Santos <agusgarcia3007@gmail.com>
2021-10-25 21:31:11 -0300

    styled components template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import image from './cryptocurrency.png';
+import Form from './components/Form';
+
+
+
+const Container = styled.div `
+  max-width:900px;
+  margin: 0 auto;
+  @media(min-width:992px){
+    display:grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap:2rem;
+
+  }
+`
+const Image = styled.img `
+  max-width:100%;
+  margin-top:5rem;
+
+`
+const Heading = styled.h1 `
+  font-family:'Bebas Neue', cursive;
+  color:#fff;
+  text-align: left;
+  font-weight: 700;
+  font-size:50px;
+  margin-bottom: 50px;
+  margin-top:80px;
+
+  &::after{
+    content:'';
+    width:100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+    `
 
 
 const App = () => {
   return ( 
-    <h1>App</h1>
+    <Container>
+      <div>
+        <Image 
+          src={image}
+          alt='crypto png'
+        />
+      </div>
+      <div>
+        <Heading>
+          Crypto Currency Chart
+        </Heading>
+
+          <Form 
+          
+          />
+      </div>
+    </Container>
    );
 }
  
